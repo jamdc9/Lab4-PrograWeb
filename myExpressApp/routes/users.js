7bd0a8aa4f0274535901a47cf841f5969e410d16
jamdc9 <jamdc9@gmail.com>
2018-09-30 20:52:44 -0600

    Commit Ejercicio 4

---
+++ myExpressApp/routes/users.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  var data = "{ \"hello\": \""+req.query.name+"\"}";
+  res.send(data);
+});
+
+module.exports = router;
